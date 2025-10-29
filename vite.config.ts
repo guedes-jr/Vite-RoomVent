1e278e1710398d97e5a51c40da39e62c414559cf
João Guedes <joao.guedes.developer@gmail.com>
2025-10-29 01:04:13 -0300

    ajustes para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Vite-RoomVent/", // ✅ Caminho correto para GitHub Pages de projeto
   server: {
     host: "::",
     port: 8080,
